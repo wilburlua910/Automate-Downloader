a5fe7ec9c2d61682106dd28b8c04d393e7e8b0f3
wilburlua910 <wilburlua910@outlook.com>
2024-05-09 22:02:40 +0800

    Added FireFox Selenium class

---
+++ run.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import os 
+
+class FireFoxBinariesDownloader:
+
+    def __init__(self):
+        options = webdriver.FirefoxOptions()
+        options.add_argument('')
+        self.driver = webdriver.Firefox()
+
+    def downloadNodeJS():
+        driver = webdriver.Firefox()
+
+        driver.get('https://nodejs.org/en')
+
+        button = driver.find_element(By.LINK_TEXT, 'Download Node.js (LTS)')
+        button.click()
+        driver.close()
+
+    def SetUpBrowser():
+        options = webdriver.FirefoxOptions()
+        downloadsPath = "/home/downloads"
+        os.makedirs(downloadsPath)
+    
+    def downloadOracle():
+        driver = webdriver.Firefox()
+        driver.get('https://www.oracle.com/my/java/technologies/downloads/#java11-windows')
+
+        javaBtnElement = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID, 'rt01tab1-jdk22-windows'))
+
+        if javaBtnElement is not None:
+            print('Able to locate java 22 button')
+            javaBtnElement.click()
+        else:
+            print('Unable to locate java 22 button')
+
+        downloadWindows = driver.find_elements(By.CSS_SELECTOR, 'a')
+
+        for a in downloadWindows:
+            print(a.get_attribute('href'))
+
+            if a.get_attribute('href') == 'https://download.oracle.com/java/22/latest/jdk-22_linux-aarch64_bin.tar.gz':
+                driver.get(a.get_attribute('href'))
+                
+if __name__ == "__main__":
+    #Main method to create objects
+    pass
